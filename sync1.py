f53c74e22f9afd0683a3cfa8942527caf81b6130
Youzhi Wang <youzhiwang2001@gmail.com>
2024-10-24 13:17:51 -0400

    batman

---
+++ sync1.py
@@ -0,0 +1,61 @@
+import json
+
+import requests
+import time  # Import time to measure execution duration
+
+urls = [
+    {"rel": "users", "href": ""},
+    {"rel": "product", "href": ""},
+    {"rel": "order", "href": ""}
+]
+
+
+def call_get(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        return response.json()  # Return the JSON response
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+        return None
+    except Exception as err:
+        print(f"An error occurred: {err}")
+        return None
+
+
+def call_get_urls(urls):
+
+    result = {}
+
+    for u in urls:
+        r = call_get(u["href"])
+        t = r.get(u["rel"],r)
+        result[u["rel"]] = t
+
+    return result
+
+# Example usage:
+def main():
+
+    # Start measuring time
+    start_time = time.time()
+
+    # Call API three times synchronously
+    full_result = call_get_urls(
+        urls
+    )
+
+    # Calculate total execution time
+    total_time = time.time() - start_time
+
+    # Print all the responses
+    print("The full response = \n",
+          json.dumps(full_result, indent=2))
+
+    # Print total execution time
+    print(f"Total execution time: {total_time:.2f} seconds")
+
+
+# To run the program
+if __name__ == "__main__":
+    main()
